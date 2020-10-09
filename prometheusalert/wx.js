126b8297c7cf00beb80e949efd02e3926131ba6e
se7vet <17740830+se7vet@users.noreply.github.com>
2020-10-09 14:52:51 +0800

    Update wx.js

--- prometheusalert/wx.js
+++ prometheusalert/wx.js
@@ -1,11 +1,8 @@
 const timezone = require('moment-timezone')
-
 exports.template = function(body) {
     //企业微信群机器人API，https://work.weixin.qq.com/help?person_id=1&doc_id=13376#markdown%E7%B1%BB%E5%9E%8B
     //prometheus alert manager webhook ： https://prometheus.io/docs/alerting/configuration/#webhook_config
     const alerts = body.alerts;
-
-
     const content = alerts.map(
         alert => {
             return [`【运维中心】-〔k8s监控〕-> <font color="${body.status === 'firing' ? 'warning' : 'info'}">${body.status}</font>`]
@@ -15,7 +12,6 @@ exports.template = function(body) {
                 .join("\n")
         }
     ).join("\n\n");
-
     return {
 
         msgtype: "markdown",
